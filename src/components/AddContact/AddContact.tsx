074f9750fe37d98cc0ee96c2341751f29d6afa2a
Izzet <izzetertas@gmail.com>
2019-06-28 00:55:08 +0100

    AddContact component

---
+++ src/components/AddContact/AddContact.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Mutation } from 'react-apollo'
+
+import { ADD_CONTACT } from '../queries';
+import ContactEntryForm from '../ContactEntryForm/ContactEntryForm';
+
+export interface AddContactProps {
+  onSubmit: (isRecordAdded: boolean) => void,
+  history: any,
+}
+
+export default function AddContact(props: AddContactProps) {
+  
+  return (
+    <Mutation
+      mutation={ADD_CONTACT}
+    >
+      {addTodo => (
+        <ContactEntryForm
+          onSubmit={(contact: any) => {
+            console.log(contact)
+            if (contact) addTodo({ variables: { contact } })
+            props.history.push('/')
+          }
+          }
+        />
+      )}
+    </Mutation>
+  )
+}
