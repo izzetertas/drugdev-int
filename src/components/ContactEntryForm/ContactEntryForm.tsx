ce695f705173efd5f72ec7532efc858545088fdf
Izzet <izzetertas@gmail.com>
2019-06-28 09:16:18 +0100

    design improvements

--- src/components/ContactEntryForm/ContactEntryForm.tsx
+++ src/components/ContactEntryForm/ContactEntryForm.tsx
@@ -5,6 +5,8 @@ import Button from '@material-ui/core/Button';
 import { FormControl, Input, InputLabel, Theme, Grid } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/styles';
 
+import './ContactEntryForm.css'
+
 export interface ContactInfo {
   id: string,
   name: string,
@@ -31,57 +33,62 @@ export default function ContactEntryForm(props: ContactEntryFormProps) {
   const [name, setName] = useState(get(props, 'contactInfo.name', ''))
 
   return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          props.onSubmit({
-            id: get(props, 'contactInfo.id', uuid()),
-            name,
-            email
-          })
-        }}
-      >
-        <div>
-          <FormControl>
-            <InputLabel htmlFor="name">Name</InputLabel>
-            <Input
-              id="name"
-              aria-describedby="name"
-              onChange={(e) => setName(e.target.value)}
-              defaultValue={name}
-            />
-          </FormControl>
-        </div>
-        <div>
-          <FormControl>
-            <InputLabel htmlFor="email">Email address</InputLabel>
-            <Input
-              id="email"
-              aria-describedby="email"
-              onChange={(e) => setEmail(e.target.value)}
-              defaultValue={email}
-            />
-          </FormControl>
-        </div>
-        <div>
-          <Button type='submit'
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            {get(props, 'contactInfo.id') ? 'Update' : 'Add'}
+    <div className='contactEntryWrapper'>
+      <div className='contactEntryHeader'>
+        <h2> {get(props, 'contactInfo.id') ? 'Update' : 'Add New'} Contact</h2>
+      </div>
+      <div className='contactEntryContainer'>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            props.onSubmit({
+              id: get(props, 'contactInfo.id', uuid()),
+              name,
+              email
+            })
+          }}
+        >
+          <div className='formItem'>
+            <FormControl>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <Input
+                id="name"
+                aria-describedby="name"
+                onChange={(e) => setName(e.target.value)}
+                defaultValue={name}
+              />
+            </FormControl>
+          </div>
+          <div>
+            <FormControl>
+              <InputLabel htmlFor="email">Email address</InputLabel>
+              <Input
+                id="email"
+                aria-describedby="email"
+                onChange={(e) => setEmail(e.target.value)}
+                defaultValue={email}
+              />
+            </FormControl>
+          </div>
+          <div className='buttonContainer'>
+            <Button type='submit'
+              variant="contained"
+              color="primary"
+              className='button'
+            >
+              {get(props, 'contactInfo.id') ? 'Update' : 'Add'}
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className='button'
+              onClick={() => props.onSubmit(null)}
+            >
+              Cancel
           </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            onClick={() => props.onSubmit(null)}
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
     </div >
   )
 }
